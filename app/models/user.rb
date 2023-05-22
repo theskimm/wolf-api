08552507fa13647838bbe816a62f01e9d12374fa
Venecia Alvarez <venecia.alvarez@theskimm.com>
2023-05-22 11:11:04 -0500

    Delete stuff

--- app/models/user.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :birthdate, presence: true
-  validates :city, presence: true
-  validates :state, presence: true
-end
